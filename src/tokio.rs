5b43de41f83e4fef22b9ecd53f3490b1bb178b5b
Alexander Polakov <alex@plhk.dev>
2023-08-24 11:09:01 +0400

    test: add tokio test

--- src/tokio.rs
+++ src/tokio.rs
@@ -35,15 +35,17 @@ impl<'a> Future for SendFd<'a> {
         let this = Pin::into_inner(self);
         let stream_fd = this.stream.as_raw_fd();
 
-        ready!(this.stream.poll_write_ready(cx))?;
+        loop {
+            ready!(this.stream.poll_write_ready(cx))?;
 
-        let res = this
-            .stream
-            .try_io(Interest::WRITABLE, || stream_fd.send_fd(this.fd));
-        match res {
-            Ok(_) => Poll::Ready(Ok(())),
-            Err(err) if err.kind() == ErrorKind::WouldBlock => Poll::Pending,
-            Err(err) => Poll::Ready(Err(err)),
+            let res = this
+                .stream
+                .try_io(Interest::WRITABLE, || stream_fd.send_fd(this.fd));
+            match res {
+                Ok(_) => break Poll::Ready(Ok(())),
+                Err(err) if err.kind() == ErrorKind::WouldBlock => continue,
+                Err(err) => break Poll::Ready(Err(err)),
+            }
         }
     }
 }
@@ -59,16 +61,18 @@ impl<'a> Future for RecvFd<'a> {
         let this = Pin::into_inner(self);
         let stream_fd = this.stream.as_raw_fd();
 
-        ready!(this.stream.poll_read_ready(cx))?;
+        loop {
+            ready!(this.stream.poll_read_ready(cx))?;
 
-        let res = this
-            .stream
-            .try_io(Interest::READABLE, || stream_fd.recv_fd());
+            let res = this
+                .stream
+                .try_io(Interest::READABLE, || stream_fd.recv_fd());
 
-        match res {
-            Ok(val) => Poll::Ready(Ok(val)),
-            Err(err) if err.kind() == ErrorKind::WouldBlock => Poll::Pending,
-            Err(err) => Poll::Ready(Err(err)),
+            match res {
+                Ok(val) => break Poll::Ready(Ok(val)),
+                Err(err) if err.kind() == ErrorKind::WouldBlock => continue,
+                Err(err) => break Poll::Ready(Err(err)),
+            }
         }
     }
 }
@@ -80,5 +84,71 @@ impl FdPassingExt for UnixStream {
 
     fn recv_fd(&self) -> RecvFd {
         RecvFd { stream: self }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::FdPassingExt;
+    use std::fs::File;
+    use std::io::Read;
+    use std::os::fd::{AsRawFd, FromRawFd};
+    use tempdir::TempDir;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use tokio::net::{UnixListener, UnixStream};
+    use tokio::runtime::Builder;
+
+    #[test]
+    fn async_it_works() {
+        let tmp_dir = TempDir::new("passfd").unwrap();
+        let sock_path = tmp_dir.path().join("listener.sock");
+
+        match unsafe { libc::fork() } {
+            -1 => panic!("fork went wrong"),
+            0 => {
+                println!("child process, wait for socket to appear");
+                let rt = Builder::new_current_thread().enable_all().build().unwrap();
+                rt.block_on(async {
+                    for _ in 0..10 {
+                        match UnixStream::connect(sock_path.clone()).await {
+                            Ok(mut stream) => {
+                                println!("stream connected");
+                                let fd = stream.recv_fd().await?;
+                                println!("fd received");
+                                let mut file = unsafe { File::from_raw_fd(fd) };
+                                let mut buf = String::new();
+                                file.read_to_string(&mut buf)?;
+                                stream.write_all("ok".as_bytes()).await?;
+                                break;
+                            }
+                            Err(e) => {
+                                println!("not connected {:?}", e);
+                                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                            }
+                        }
+                    }
+                    Ok::<_, std::io::Error>(())
+                })
+                .unwrap();
+            }
+            _ => {
+                println!("parent, start listening");
+                let rt = Builder::new_current_thread().enable_all().build().unwrap();
+                rt.block_on(async {
+                    let listener = UnixListener::bind(sock_path)?;
+                    println!("opening file");
+                    let file = File::open("/etc/passwd")?;
+                    let (mut stream, _) = listener.accept().await?;
+                    println!("client connected, sending fd");
+                    stream.send_fd(file.as_raw_fd()).await?;
+                    let mut buf = String::new();
+                    stream.read_to_string(&mut buf).await?;
+                    println!("got response {:?}", buf);
+                    assert!(buf == "ok");
+                    Ok::<_, std::io::Error>(())
+                })
+                .unwrap();
+            }
+        }
     }
 }
